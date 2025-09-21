db595e3fd57d63c612baac1dd7d0c11f02608d9d
Rishikesh <rishisoorya94@gmail.com>
2025-09-22 00:22:48 +0530

    Terraform EC2 instance creation-v1.0

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -3,6 +3,15 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+    ami           = "ami-0360c520857e3138f"  # Specify an appropriate AMI ID
+    instance_type = "t3.micro"
+    subnet_id = "subnet-04106d7952f64b59d"
+    key_name = "forMobaXtermKey"
+
+
+tags = {
+
+    Name = "EC2 from VS-terraform"
 }
+}
+
